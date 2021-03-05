616375d4ee1af7bc4a34a43ed1963d9f0b9bcd4a
maf-o <60689016+maf-o@users.noreply.github.com>
2021-03-05 21:02:54 +0100

    powersehll gridview free space

---
+++ gridview_diskdrive.ps1
@@ -0,0 +1,50 @@
+﻿#Got script from https://mcpmag.com/articles/2016/02/17/creating-a-gui-using-out-gridview.aspx
+
+
+$WMI =  @{
+
+  Filter =  "DriveType='3' AND (Not Name LIKE  '\\\\?\\%')"
+
+  Class =  "Win32_Volume"
+
+  ErrorAction =  "Stop"
+
+  Property =  "Name","Label","Capacity","FreeSpace"
+
+  Computername =  $Env:COMPUTERNAME
+
+}
+
+$List = New-Object System.Collections.ArrayList
+
+  Get-WmiObject @WMI  | ForEach {
+
+  $Decimal  = $_.freespace / $_.capacity
+
+  $Graph  = "$($Bar)"*($Decimal*100)
+
+  $Hash = [ordered]@{
+
+  Computername =  $Env:COMPUTERNAME
+
+  Name =  $_.Name
+
+  FreeSpace =  "$Graph"       
+
+  Percent =  $Decimal
+
+  FreeSpaceGB =  ([math]::Round(($_.Freespace/1GB),2))
+
+  CapacityGB =  ([math]::Round(($_.Capacity/1GB),2))
+
+  }
+
+  [void]$List.Add((
+
+  [pscustomobject]$Hash
+
+  ))
+
+  }
+
+$List | Out-GridView -Title 'Drive Space' 
